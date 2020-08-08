2573d31c6366b52586782a3233eb51a876e40c1b
Kaustav Haldar <hi@kaustav.me>
2020-08-08 17:22:59 -0400

    cleanup script

--- main.js
+++ main.js
@@ -3,21 +3,20 @@ const { createPow } = pgc;
 
 const fs = require('fs');
 
-// const host = "http://0.0.0.0:6002" // or whatever powergate instance you want
-const host = "http://pow.hackfs.textile.io:6002" // or whatever powergate instance you want
+// $ while sleep 100; do   node main.js; done
+const host = "http://0.0.0.0:6002" // or whatever powergate instance you want
 
 const pow = createPow({ host })
 main()
 
 async function main() {
-    const filesyncpath = 'peersync3.json'
+    const filesyncpath = 'peersync4.json'
     const { peersList } = await pow.net.peers()
     
     console.log(peersList)
 
     let jsondata = fs.existsSync(filesyncpath) ? JSON.parse(fs.readFileSync(filesyncpath)) : [];
-    jsondata = jsondata.concat(peersList)
-    jsondata = dedupe(jsondata)
+    jsondata = dedupe(jsondata.concat(peersList))
 
     fs.writeFileSync(filesyncpath, JSON.stringify(jsondata), 'utf8');
 }
@@ -25,9 +24,8 @@ async function main() {
 function dedupe(arr) {
     let set = {};
     let res = [];
-    arr.forEach((ele, index) => {
+    arr.forEach((ele) => {
         let id = ele.addrInfo.id;
-        // console.log(ele)
         if (!set[id]) {
             set[id] = true;
             res.push(ele)
